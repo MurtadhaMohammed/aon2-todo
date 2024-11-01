e33917a0379eacbffe721c2c255f5b3b3988695f
MurtadhaMohammed <mumu9943@gmail.com>
2024-11-01 15:21:01 +0300

    first commit

---
+++ src/screens/About/about.jsx
@@ -0,0 +1,11 @@
+import Container from "../../components/Container/container";
+
+const AboutScreen = () => {
+  return (
+    <div>
+      <Container>About Screen</Container>
+    </div>
+  );
+};
+
+export default AboutScreen
